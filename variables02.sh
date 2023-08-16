7990790834c8e348b86ca8827f46c83936cff527
Rahul <rahuln8897@gmail.com>
2023-08-16 20:32:18 +0530

    Using variables doing some practice on script

---
+++ variables02.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hello World"
+
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hi $Person2"
+echo "$Person2: Hi $Person1"
+echo "$Person1: How are you $Person2"
+echo "$Person2: I am fine what about you $Person1"
