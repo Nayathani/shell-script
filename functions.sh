6887c4f65adf5e20b820dae71b9f79fa201353dd
Rahul <rahuln8897@gmail.com>
2023-08-17 13:00:08 +0530

    installing mysql server

---
+++ functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo "Installation...FAILURE"
+    exit 1
+else
+    echo "Installation...SUCCESS"
+fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+fi
+
+yum install mysql -y 
+
+VALIDATE $?
+
+yum install postfix -y
+
+VALIDATE $?
