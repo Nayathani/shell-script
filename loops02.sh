1e84770b7b3343222efd172fbea4a8954932c63f
Rahul <rahuln8897@gmail.com>
2023-08-17 19:23:28 +0530

    loops practice using validations

--- loops02.sh
+++ loops02.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo -e "$2... $R FAILURE $N"
+    exit 1
+else
+    echo -e "$2... $G SUCCESS $N"
+fi
+}
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1
+fi
+
 for i in $@
 do
-    yum install $i -y
+    yum install $i -y &>>$LOGFILE
+    
+    VALIDATE $? "Installing $i"
 done
