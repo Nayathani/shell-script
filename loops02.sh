24c47f0d7dd7216dab71b3f72cef22dfe8f21908
Rahul <rahuln8897@gmail.com>
2023-08-18 11:29:26 +0530

    loops Practice using validate functions

--- loops02.sh
+++ loops02.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 
 DATE=$(date +%F)
+LOGSDIR=/home/centos/shellscript-logs
 SCRIPT_NAME=$0
-LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+LOGFILE=$LOGSDIR/$0-$DATE.log
 
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-
-VALIDATE(){
-if [ $1 -ne 0 ]
-then
-    echo -e "$2... $R FAILURE $N"
-    exit 1
-else
-    echo -e "$2... $G SUCCESS $N"
-fi
-}
+Y="\e[33m"
 
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR:: Please run this script with root access"
+    echo "$R ERROR:: Please run this script with root access $N"
     exit 1
 fi
 
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo -e "$R Installing $2..... $R FAILURE $N"
+    exit 1
+else
+    echo -e "$R Installing $2..... $G SUCCESS $N"
+fi
+}
+
+
 for i in $@
 do
-    yum install $i -y &>>$LOGFILE
-    
-    VALIDATE $? "Installing $i"
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0]
+    then
+        echo "$i is not installed, let's install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else
+        echo -e "$Y $i is already installed $N"
+    fi
+
 done
